e0b217f89d9a66926354081491008165e8396992
shuvamb1 <shuvambhattacharya896@gmail.com>
2025-08-13 20:37:41 +0530

    Update server.js

--- poll-system/server.js
+++ poll-system/server.js
@@ -9,6 +9,7 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -85,4 +86,5 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
